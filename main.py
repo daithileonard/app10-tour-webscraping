c1ebc415d7ea729405e0e08362ada363933ea049
daithileonard <DAITHILEONARD@GMAIL.COM>
2023-05-11 10:21:57 +0100

    get content from website

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-# This is a sample Python script.
+import requests
+import selectorlib
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+URL = "https://programmer100.pythonanywhere.com/tours/"
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def scrape(url):
+    """Scrape the page source from the URL"""
+    response = requests.get(url)
+    source = response.text
+    return source
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+if __name__ == "__main__":
+    print(scrape(URL))
